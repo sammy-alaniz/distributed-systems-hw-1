aa2134b00839f56b458c9c36d1a1b4c13a303626
Sammy <samalaniz25@gmail.com>
2022-09-06 22:23:33 -0500

    moved txt file into a hash map

--- q4/Server.java
+++ q4/Server.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class Server {
 
@@ -33,39 +34,39 @@ public class Server {
     }
   }
 
-  public static void main (String[] args) throws FileNotFoundException, IOException{
-    int tcpPort;
-    int udpPort;
-    if (args.length != 3) {
-      System.out.println("ERROR: Provide 3 arguments");
-      System.out.println("\t(1) <tcpPort>: the port number for TCP connection");
-      System.out.println("\t(2) <udpPort>: the port number for UDP connection");
-      System.out.println("\t(3) <file>: the file of inventory");
-
-      System.exit(-1);
-    }
-    tcpPort = Integer.parseInt(args[0]);
-    udpPort = Integer.parseInt(args[1]);
-    String fileName = args[2];
-
-
-
+  public static Map parseInventoryFile(String filePath) throws FileNotFoundException, IOException
+  {
     // parse the inventory file
     // Geeks for Geeks : https://www.geeksforgeeks.org/bufferedreader-readline-method-in-java-with-examples/
-    FileReader fileReader = new FileReader(fileName);
+    FileReader fileReader = new FileReader(filePath);
     BufferedReader buffReader = new BufferedReader(fileReader);
+    Map<String,Integer> inventoryMap = new HashMap<String,Integer>();
     while (buffReader.ready())
     {
-      System.out.println(buffReader.readLine());
+      String readLine = buffReader.readLine();
+      String[] dataItem = readLine.split(" ");
+      if(dataItem.length > 1)
+      {
+        inventoryMap.put(dataItem[0], Integer.parseInt(dataItem[1]));
+      }
     }
 
+    for(Map.Entry<String,Integer> entry : inventoryMap.entrySet())
+    {
+      System.out.println(entry.getKey() + " " + Integer.toString(entry.getValue()));
+    }
+    return inventoryMap;
+  }
+
+  public static void udpServer(Integer port)
+  {
     // TODO: handle request from clients
     // Chapter 6 : Distributed Programming
     int len = 1024;
     DatagramPacket dataPacket, returnPacket;
     try
     {
-      DatagramSocket dataSocket = new DatagramSocket(udpPort);
+      DatagramSocket dataSocket = new DatagramSocket(port);
       byte[] buf = new byte[len];
       while(true)
       {
@@ -92,5 +93,30 @@ public class Server {
     {
       System.err.println((se));
     }
+  }
+
+  public static void main (String[] args) throws FileNotFoundException, IOException{
+    int tcpPort;
+    int udpPort;
+    if (args.length != 3) {
+      System.out.println("ERROR: Provide 3 arguments");
+      System.out.println("\t(1) <tcpPort>: the port number for TCP connection");
+      System.out.println("\t(2) <udpPort>: the port number for UDP connection");
+      System.out.println("\t(3) <file>: the file of inventory");
+
+      System.exit(-1);
+    }
+    tcpPort = Integer.parseInt(args[0]);
+    udpPort = Integer.parseInt(args[1]);
+    String fileName = args[2];
+
+    // TODO: getters and setters for inventory map, how do we make this thread safe?
+    Map<String,Integer> inventoryMap = parseInventoryFile(fileName);
+
+    udpServer(udpPort);
+
+
+
+
   }
 }
