628b48aa25ef60195ae95da5cedc18ca0c1e7444
Sammy <samalaniz25@gmail.com>
2022-09-07 22:52:22 -0500

    made a udp server, command parser, and inventory class

---
+++ q4/CommandParser.java
@@ -0,0 +1,45 @@
+import Inventory.*;
+
+public class CommandParser {
+
+    public CommandParser(Inventory argInventory)
+    {
+        mInventroy = argInventory;
+    }
+
+    Inventory mInventroy;
+
+    public byte[] parseBuffer(byte[] msgData) 
+    {
+        String msg = new String(msgData);
+        String[] tokens = msg.split(" ");
+    
+        System.out.print(msg);
+    
+        if (tokens[0].equals("setmode")) {
+            System.out.println("setmode hit");
+          // TODO: set the mode of communication for sending commands to the server 
+          // and display the name of the protocol that will be used in future
+        }
+        else if (tokens[0].equals("purchase")) {
+
+            mInventroy.buyItem(tokens[2],Integer.parseInt(tokens[3].trim()));
+
+          // TODO: send appropriate command to the server and display the
+          // appropriate responses form the server
+        } else if (tokens[0].equals("cancel")) {
+          // TODO: send appropriate command to the server and display the
+          // appropriate responses form the server
+        } else if (tokens[0].equals("search")) {
+          // TODO: send appropriate command to the server and display the
+          // appropriate responses form the server
+        } else if (tokens[0].equals("list")) {
+          // TODO: send appropriate command to the server and display the
+          // appropriate responses form the server
+        } else {
+            System.out.println("ERROR: No such command");
+        }
+
+        return msgData;
+    }
+}
--- q4/Server.java
+++ q4/Server.java
@@ -5,74 +5,6 @@ import java.util.*;
 import Inventory.*;
 
 public class Server {
-
-  public static void parseBuffer(byte[] msgData) {
-    String msg = new String(msgData);
-    String[] tokens = msg.split(" ");
-
-    System.out.print(msg);
-
-    if (tokens[0].equals("setmode")) {
-      System.out.println("setmode hit");
-      // TODO: set the mode of communication for sending commands to the server 
-      // and display the name of the protocol that will be used in future
-    }
-    else if (tokens[0].equals("purchase")) {
-      System.out.println("purchase hit");
-      System.out.println(msg);
-      // TODO: send appropriate command to the server and display the
-      // appropriate responses form the server
-    } else if (tokens[0].equals("cancel")) {
-      // TODO: send appropriate command to the server and display the
-      // appropriate responses form the server
-    } else if (tokens[0].equals("search")) {
-      // TODO: send appropriate command to the server and display the
-      // appropriate responses form the server
-    } else if (tokens[0].equals("list")) {
-      // TODO: send appropriate command to the server and display the
-      // appropriate responses form the server
-    } else {
-      System.out.println("ERROR: No such command");
-    }
-  }
-
-  public static void udpServer(Integer port)
-  {
-    // TODO: handle request from clients
-    // Chapter 6 : Distributed Programming
-    int len = 1024;
-    DatagramPacket dataPacket, returnPacket;
-    try
-    {
-      DatagramSocket dataSocket = new DatagramSocket(port);
-      byte[] buf = new byte[len];
-      while(true)
-      {
-        try
-        {
-          dataPacket = new DatagramPacket(buf, buf.length);
-          dataSocket.receive(dataPacket);
-
-          parseBuffer(dataPacket.getData());
-
-          returnPacket = new DatagramPacket(dataPacket.getData(),
-                                            dataPacket.getLength(),
-                                            dataPacket.getAddress(),
-                                            dataPacket.getPort());
-          dataSocket.send(returnPacket);
-        }
-        catch (IOException e)
-        {
-          System.err.println(e);
-        }
-      }
-    }
-    catch (SocketException se)
-    {
-      System.err.println((se));
-    }
-  }
-
   public static void main (String[] args) throws FileNotFoundException, IOException{
     int tcpPort;
     int udpPort;
@@ -91,9 +23,17 @@ public class Server {
     // TODO: getters and setters for inventory map, how do we make this thread safe?
 
 
+
+
     //udpServer(udpPort);
     Inventory inventory = new Inventory(fileName);
     inventory.buyItem("xbox", 3);
     inventory.printMap();
+
+    CommandParser serverParser = new CommandParser(inventory);
+
+
+    UDPServer server = new UDPServer(udpPort, serverParser);
+    server.start();
   }
 }
